fa67c548c607d9ffd6acff76d060e508b6cb3bf2
Mark Ryan <markrryan99@gmail.com>
2023-06-21 14:18:09 -0700

    added a sidemenu/links

--- app.js
+++ app.js
@@ -74,3 +74,24 @@ restartBTN.addEventListener("click", () => {
 });
 
 gameSquares();
+
+// Sidebar Javascript
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const sideMenu = document.querySelector('.side-menu');
+const burger = document.querySelectorAll('.hamburger')
+console.log(burger)
+
+hamburgerMenu.addEventListener('click', function() {
+  hamburgerMenu.classList.toggle('active');
+  sideMenu.classList.toggle('active');
+  // hamburgerMenu.style.display = 'none'
+});
+
+document.addEventListener('click', function(event) {
+  const target = event.target;
+  if (!target.closest('.side-menu-container')) {
+    hamburgerMenu.classList.remove('active');
+    sideMenu.classList.remove('active');
+    // hamburgerMenu.style.display = 'block'
+  }
+});
